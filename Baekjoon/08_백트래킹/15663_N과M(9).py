b3581d30bb44635186db7790bf44a0c26a7471c7
박사랑 <sarangx1227@gmail.com>
2025-09-11 17:58:39 +0900

    UPDATE

--- Baekjoon/08_백트래킹/15663_N과M(9).py
+++ Baekjoon/08_백트래킹/15663_N과M(9).py
@@ -2,16 +2,25 @@ import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
 N_list = list(map(int, input().split()))
-N_list.sort()
+# N_list.sort()
 
 result = []
+total = set()
 def func(now, cnt):
+    temp = []
     if cnt == M:
-        print(' '.join(result))
+        for p in result:
+            temp.append(N_list[p])
+        temp = tuple(temp)
+        total.add(temp)
         return
     for idx in range(now, len(N_list)):
-        result.append(f'{N_list[idx]}')
+        if idx in result:
+            continue
+        # result.append(f'{N_list[idx]}')
+        result.append(idx)
         func(idx, cnt + 1)
         result.pop()
 
 func(0, 0)
+print(total.sorted())
