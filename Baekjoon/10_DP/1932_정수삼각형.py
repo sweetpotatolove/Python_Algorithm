29fba976105c887685dd8aca12183e7a775072c1
박사랑 <sarangx1227@gmail.com>
2025-09-18 17:56:54 +0900

    2025.09.18 문제풀이

--- Baekjoon/10_DP/1932_정수삼각형.py
+++ Baekjoon/10_DP/1932_정수삼각형.py
@@ -2,9 +2,19 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-numbers = [num for _ in range(n) for num in map(int, input().split())]
+# numbers = [num for _ in range(n) for num in map(int, input().split())]
+numbers = [list(map(int, input().split())) for _ in range(n)]
 
-memo = [[0, 0] for i in range(1, n+1) for _ in range(i)]
+memo = [[0] * i for i in range(1, n+1)]
+memo[0][0] = numbers[0][0]
+for i in range(1, n):
+    for j in range(len(numbers[i])):
+        if j == 0:
+            memo[i][j] = numbers[i][j] + memo[i-1][0]
+        elif j == (len(numbers[i]) - 1):
+            memo[i][j] = numbers[i][j] + memo[i-1][j-1]
+        else:
+            memo[i][j] = numbers[i][j] + max(memo[i-1][j-1], memo[i-1][j])
+        
 
-print(numbers)
-print(memo)
+print(max(memo[-1]))
